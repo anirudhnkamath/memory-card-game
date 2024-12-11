bd505c1f80585e07ed7d802cd2dc99845d2a7ec0
Anirudh N Kamath <anirudhnkamath@Anirudhs-MacBook-Air.local>
2024-12-11 15:45:48 +0530

    added maximum 2 cards can be flipped at once logic

--- src/app.js
+++ src/app.js
@@ -12,26 +12,47 @@ export default function App() {
     return arr;
   }
 
+  function flipLogic(index){
+    console.log(active);
+
+    //if already 2 active and new is flipped
+    if(!active.includes(index) && active.length >= maxActive){
+      console.log(1);
+      for(const i of active) flipCard(i);
+      setTimeout(() => {
+        flipCard(index);
+      }, 200);
+      setActive([index]);
+    }
+
+    //if already flipped card is flipped
+    else if(active.includes(index)){
+      console.log(2);
+      flipCard(index);
+      const newActive = active.filter(i => i !== index);
+      setActive(newActive);
+    }
+
+    else if(!active.includes(index) && active.length < maxActive){
+      console.log(3);
+      flipCard(index);
+      setActive(prevActive => [...prevActive, index]);
+    }
+  }
+
   function flipCard(index){
-    if(isFlipped[index]){
-      gsap.to(`.card${index}`, {
-        rotateY: "0",
-        duration: 0.6
-      })
-    }
-    else {
-      gsap.to(`.card${index}`, {
-        rotateY: "180deg",
-        duration: 0.6
-      })
-    }
+    gsap.to(`.card${index}`, {
+      rotateY: isFlipped[index]? "0": "180deg",
+      duration: 0.3,
+      overwrite: true
+    });
 
-    const newFlipped = isFlipped.map((item,itemIndex) => {
-      if(itemIndex === index) return !item;
-      return item
-    })
-
-    setIsFlipped(newFlipped);
+    setIsFlipped((prevFlipped) => {
+      return prevFlipped.map((item, itemIndex) => {
+        if(itemIndex === index) return !item;
+        return item;
+      })
+    });
   }
 
   const [letters, setLetters] = React.useState(
@@ -40,14 +61,16 @@ export default function App() {
   const [isFlipped, setIsFlipped] = React.useState(
     [false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false]
   );
+  const [active, setActive] = React.useState([]);
+  const maxActive = 2;
 
   const cards = letters.map((letter, index) => {
-    return <Card key={index} value={letter} className={`card${index}`} onClick={() => flipCard(index)}/>;
+    return <Card key={index} value={letter} className={`card${index}`} onClick={() => flipLogic(index)}/>;
   });
 
   return (
     <section className="flex justify-center items-center h-screen bg-gray-800">
-      <section className="grid grid-cols-4 gap-[1em] text-white p-4">
+      <section className="grid grid-cols-4 gap-[10px] text-white p-4">
         {cards}
       </section>
     </section>
--- src/components/card.js
+++ src/components/card.js
@@ -4,7 +4,7 @@ export default function Card(props) {
   return (
     <div className={`card-container ${props.className}`} onClick={props.onClick}>
       <div className='front'></div>
-      <div className='back'>
+      <div className='back text-3xl'>
         <h1>{props.value}</h1>
       </div>
     </div>
